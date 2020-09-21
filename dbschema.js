14df7dc9c348774cacb3f309ff3d9c83f88d9f74
GautierCo <gautier.colasse@gmail.com>
2020-09-21 04:04:28 +0200

    Project started, routes /login new student/coach, get all student by coachId

---
+++ dbschema.js
@@ -0,0 +1,37 @@
+let database = {
+    users: [
+        {
+            uid: "azSD5QS4D541d21",
+            firstname: "firstname",
+            lastname: "lastname",
+            displayName: "firstname" + " " + "lastname",
+            email: "email",
+            phone: "phone",
+
+            country: "country",
+            city: "city",
+            numberStreet: "numberStreet",
+            address: "address",
+            postalcode: "postalcode",
+            region: "region",
+            birthday: "birthday",
+
+            size: "size",
+            weight: "weight",
+            gender: "gender",
+            nbrSeance: "nbrSeance",
+            objectif: "objectif",
+            note: "note",
+
+            coachId: "coachId",
+            status: "status",
+            formule: "formule",
+            startContract: "startContract",
+            endContract: "endContract",
+            isCoach: "false || true",
+            isStudent: "false || true",
+
+            createdAt: new Date().toISOString(),
+        },
+    ],
+};
